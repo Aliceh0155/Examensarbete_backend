6ffd012db9655d66821ba459fcf608cd8c13f63e
aliceh0155 <hofverbergalice@gmail.com>
2024-11-25 15:49:22 +0100

    initial commit

---
+++ src/test/java/com/alice/examensarbete_backend/ExamensarbeteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.examensarbete_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamensarbeteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
